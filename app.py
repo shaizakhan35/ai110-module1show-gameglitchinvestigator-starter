8084cd9089b5420f07da3db3bbaf94d9c63ab8fc
Shaiza Khan <skhan35@usf.edu>
2026-06-14 19:08:05 +0500

    Fix text input not clearing on new game

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ if new_game:
     st.session_state.secret = random.randint(low, high)
     st.session_state.status = "playing"
     st.session_state.history = []
+    st.session_state[f'guess_input_{difficulty}'] = ''
     st.success("New game started.")
     st.rerun()
 
